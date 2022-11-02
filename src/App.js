b28d31fad7549808a92d9135456d9dab6a295370
raphaelbaere <raphaelbaere@id.uff.br>
2022-11-02 12:43:59 -0300

    Requisito 1 concluído

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import pokemonList from './data';
 import Pokedex from './components/Pokedex';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <Pokedex pokemonList={ pokemonList } />
+      <Switch>
+        <Route exact path="/" render={ () => <Pokedex pokemonList={ pokemonList } /> } />
+        <Route path="/about" render={ () => <About /> } />
+      </Switch>
     </div>
   );
 }
